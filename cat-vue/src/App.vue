682a52add4b1ce53013f798824916b37934a1880
Ryan Lee <lee98064@gmail.com>
2022-06-06 17:18:03 +0800

    Add cat-vue

---
+++ cat-vue/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header class="header">
+    <h1 class="nav-title"><i class="fa-solid fa-cat"></i>Animal</h1>
+
+    <ul class="nav-item">
+      <li>貓咪</li>
+      <li>狗狗</li>
+      <li>兔子</li>
+    </ul>
+    <ul class="nav-func">
+      <li><i class="fa-solid fa-user"></i></li>
+      <li><i class="fa-solid fa-bag-shopping"></i></li>
+    </ul>
+  </header>
+
+  <div class="container"></div>
+</template>
