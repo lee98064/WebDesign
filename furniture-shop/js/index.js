19b460ab0f9b7b5176ab7f1eb6e4ed4ba867cc31
Ryan Lee <lee98064@gmail.com>
2022-06-15 14:52:42 +0800

    Finish page switch

--- furniture-shop/js/index.js
+++ furniture-shop/js/index.js
@@ -4,3 +4,29 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
+
+$(document).ready(function () {
+  $(".nav-menu li").click(function (e) {
+    $(".main-container .main").each(function (index) {
+      ele = $(this);
+      ele.removeClass("main-active");
+      ele.removeClass("shadow");
+      ele.removeClass("one");
+      ele.removeClass("two");
+    });
+
+    target = $(this).data("target");
+
+    $(".main-container").prepend($(target));
+    ele = $(target);
+    ele.addClass("main-active");
+
+    ele = ele.next();
+    ele.addClass("shadow");
+    ele.addClass("one");
+
+    ele = ele.next();
+    ele.addClass("shadow");
+    ele.addClass("two");
+  });
+});
